bb159535f5b9b5ee7d2a2a95f29b30936b7d6ea0
Richard Ingwe <richardingwe5@gmail.com>
2022-04-30 01:36:41 +0100

    done with animated slider

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,14 +1,95 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Animated } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { connect } from "react-redux";
+import { COLORS, icons, SIZES } from "../constants";
+import { IconTextButton } from "../components";
 
-const MainLayout = ({ children }) => {
+const MainLayout = ({ children, isTradeModalVisible }) => {
+    const modalAnimatedValue = useRef(new Animated.Value(0))
+        .current;
+
+    useEffect(() => {
+        if (isTradeModalVisible) {
+            Animated.timing(modalAnimatedValue, {
+                toValue: 1,
+                duration: 500,
+                useNativeDriver: false
+            }).start();
+        } else {
+            Animated.timing(modalAnimatedValue, {
+                toValue: 0,
+                duration: 500,
+                useNativeDriver: false
+            }).start();
+        }
+    }, [isTradeModalVisible]);
+
+    const modalY = modalAnimatedValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: [SIZES.height, SIZES.height - 320]
+    });
+
     return (
         <View style={{
             flex: 1
         }}>
             {children}
+
+            {/* Dim background */}
+            {
+                isTradeModalVisible &&
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        backgroundColor: COLORS.transparentBlack,
+                    }}
+                    opacity={modalAnimatedValue}
+                />
+            }
+
+            {/* modal */}
+            <Animated.View
+                style={{
+                    position: 'absolute',
+                    left: 0,
+                    top: modalY,
+                    width: '100%',
+                    padding: SIZES.padding,
+                    backgroundColor: COLORS.primary
+                }}
+            >
+                <IconTextButton
+                    label='Transfer'
+                    icon={icons.send}
+                    onPress={() => console.log('Transfer')}
+                />
+                <IconTextButton
+                    label='Withdraw'
+                    icon={icons.withdraw}
+                    containerStyle={{
+                        marginTop: SIZES.base
+                    }}
+                    onPress={() => console.log('Withdraw')}
+                />
+            </Animated.View>
         </View>
     );
 };
 
-export default MainLayout;
+
+function mapStateToProps(state) {
+    return {
+        isTradeModalVisible: state.tabReducer.isTradeModalVisible
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainLayout);
