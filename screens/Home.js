996ab52776681fdffb2e1904043e77fa0cf0de01
Richard Ingwe <richardingwe5@gmail.com>
2022-05-07 18:22:37 +0100

    top cryptocurrency

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,6 @@
 import React, { useCallback } from 'react';
 import {
-    View,
-    Text
+    View, Text, FlatList, TouchableOpacity, Image
 } from 'react-native';
 
 import { connect } from "react-redux";
@@ -9,7 +8,7 @@ import { getHoldings, getCoinMarket } from "../stores/market/marketActions";
 import { useFocusEffect } from "@react-navigation/native";
 
 import MainLayout from "./MainLayout";
-import { COLORS, dummyData, icons, SIZES } from "../constants";
+import { COLORS, dummyData, FONTS, icons, SIZES } from "../constants";
 import { BalanceInfo, Chart, IconTextButton } from "../components";
 
 const Home = ({ getHoldings, getCoinMarket, myHoldings, coins }) => {
@@ -93,6 +92,105 @@ const Home = ({ getHoldings, getCoinMarket, myHoldings, coins }) => {
                 />
 
                 {/* Top Cryptocurrency */}
+                <FlatList
+                    data={coins}
+                    keyExtractor={item => item.id}
+                    contentContainerStyle={{
+                        marginTop: 30,
+                        paddingHorizontal: SIZES.padding,
+                    }}
+                    ListHeaderComponent={
+                        <View style={{
+                            marginBottom: SIZES.radius
+                        }}>
+                            <Text style={{
+                                color: COLORS.white,
+                                ...FONTS.h3,
+                                fontSize: 18
+                            }}>Top Crypto Currencies</Text>
+                        </View>
+                    }
+                    renderItem={({ item }) => {
+                        let priceColor = (item.price_change_percentage_7d_in_currency == 0) ?
+                            COLORS.lightGray3 : (item.price_change_percentage_7d_in_currency > 0) ?
+                                COLORS.lightGreen : COLORS.red;
+
+                        return (
+                            <TouchableOpacity
+                                style={{
+                                    height: 55,
+                                    flexDirection: 'row',
+                                    alignItems: 'center',
+                                    justifyContent: 'center'
+                                }}
+                            >
+                                {/* logo */}
+                                <View style={{
+                                    width: 35
+                                }}>
+                                    <Image
+                                        source={{ uri: item.image }}
+                                        style={{
+                                            height: 20,
+                                            width: 20
+                                        }}
+                                    />
+                                </View>
+
+                                {/* name */}
+                                <View style={{
+                                    flex: 1
+                                }}>
+                                    <Text style={{
+                                        color: COLORS.white,
+                                        ...FONTS.h3
+                                    }}>{item.name}</Text>
+                                </View>
+
+                                {/* figures */}
+                                <View>
+                                    <Text style={{
+                                        textAlign: 'right',
+                                        color: COLORS.white,
+                                        ...FONTS.h4
+                                    }}>$ {item.current_price}
+                                    </Text>
+
+                                    <View style={{
+                                        flexDirection: 'row',
+                                        alignItems: 'center',
+                                        justifyContent: 'flex-end'
+                                    }}>
+                                        {
+                                            item.price_change_percentage_7d_in_currency != 0 &&
+                                            <Image
+                                                source={icons.upArrow}
+                                                style={{
+                                                    height: 10,
+                                                    width: 10,
+                                                    tintColor: priceColor,
+                                                    transform: (item.price_change_percentage_7d_in_currency > 0) ? [{ rotate: '45deg' }] : [{ rotate: '125deg' }],
+                                                }}
+                                            />
+                                        }
+
+                                        <Text style={{
+                                            marginLeft: 5,
+                                            color: priceColor,
+                                            ...FONTS.body5,
+                                            lineHeight: 15
+                                        }}>{item.price_change_percentage_7d_in_currency.toFixed(2)}%</Text>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        );
+                    }}
+                    ListFooterComponent={
+                        <View style={{
+                            marginBottom: 50
+                        }} />
+                    }
+                />
             </View>
         </MainLayout>
     );
