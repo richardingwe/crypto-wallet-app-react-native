15ea118f18d18c07d9ed7a16dadf9d8abea7c57a
Richard Ingwe <richardingwe5@gmail.com>
2022-05-10 23:54:35 +0100

    market screen

--- screens/Market.js
+++ screens/Market.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     View,
-    Text
+    Text,
+    FlatList,
+    TouchableOpacity,
+    Animated,
+    Image
 } from 'react-native';
+import { connect } from "react-redux";
+import { COLORS } from "../constants";
+import { getCoinMarket } from "../stores/market/marketActions";
 import MainLayout from "./MainLayout";
 
-const Market = () => {
+
+const Market = ({ getCoinMarket, coins }) => {
+    useEffect(() => {
+        getCoinMarket();
+    }, []);
+
     return (
         <MainLayout>
-            <View>
-                <Text>Market</Text>
+            <View style={{
+                flex: 1,
+                backgroundColor: COLORS.black
+            }}>
+                {/* Header */}
+
+                {/* Tab bar */}
+
+                {/* Buttons */}
+
+                {/* MarketList */}
             </View>
         </MainLayout>
     );
 };
 
-export default Market;
+
+
+function mapStateToProps(state) {
+    return {
+        coins: state.marketReducer.coins
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getCoinMarket:
+            (currency, coinList, orderBy, sparkline,
+                priceChangePerc, perPage, page) => {
+                return dispatch(getCoinMarket(currency, coinList, orderBy, sparkline,
+                    priceChangePerc, perPage, page));
+            }
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Market);
