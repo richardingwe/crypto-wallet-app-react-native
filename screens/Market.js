202424ecc50052236271916e8ff0d8e9ba6813d7
Richard Ingwe <richardingwe5@gmail.com>
2022-05-11 10:53:57 +0100

    add line charts

--- screens/Market.js
+++ screens/Market.js
@@ -1,4 +1,4 @@
-import React, { createRef, useEffect } from 'react';
+import React, { createRef, useEffect, useRef } from 'react';
 import {
     View,
     Text,
@@ -7,6 +7,7 @@ import {
     Animated,
     Image
 } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
 import { connect } from "react-redux";
 import { HeaderBar, TextButton } from "../components";
 import { COLORS, constants, FONTS, SIZES } from "../constants";
@@ -59,6 +60,8 @@ const Market = ({ getCoinMarket, coins }) => {
         getCoinMarket();
     }, []);
 
+    const scrollX = useRef(new Animated.Value(0)).current;
+
     const renderTabBar = () => (
         <View style={{
             marginTop: SIZES.radius,
@@ -94,6 +97,115 @@ const Market = ({ getCoinMarket, coins }) => {
         </View>
     );
 
+    const renderList = () => {
+        return (
+            <Animated.FlatList
+                data={marketTabs}
+                contentContainerStyle={{
+                    marginTop: SIZES.padding
+                }}
+                horizontal
+                pagingEnabled
+                scrollEventThrottle={16}
+                snapToAlignment='center'
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={item => item.id}
+                onScroll={
+                    Animated.event([
+                        { nativeEvent: { contentOffset: { x: scrollX } } }
+                    ], {
+                        useNativeDriver: false
+                    })
+                }
+                renderItem={({ item, index }) => {
+                    return (
+                        <View style={{
+                            flex: 1,
+                            width: SIZES.width
+                        }}>
+                            <FlatList
+                                data={coins}
+                                keyExtractor={item => item.id}
+                                renderItem={({ item, index }) => {
+                                    let priceColor = (item.price_change_percentage_7d_in_currency == 0) ?
+                                        COLORS.lightGray3 : (item.price_change_percentage_7d_in_currency > 0) ?
+                                            COLORS.lightGreen : COLORS.red;
+
+                                    return (
+                                        <View style={{
+                                            flexDirection: 'row',
+                                            paddingHorizontal: SIZES.padding,
+                                            marginBottom: SIZES.radius
+                                        }}>
+                                            {/* coins section */}
+                                            <View style={{
+                                                flex: 1.5,
+                                                flexDirection: 'row',
+                                                alignItems: 'center'
+                                            }}>
+                                                <Image
+                                                    source={{
+                                                        uri: item.image
+                                                    }}
+                                                    style={{
+                                                        height: 20,
+                                                        width: 20
+                                                    }}
+                                                />
+
+                                                <Text style={{
+                                                    marginLeft: SIZES.radius,
+                                                    color: COLORS.white,
+                                                    ...FONTS.h3
+                                                }}>
+                                                    {item.name}
+                                                </Text>
+                                            </View>
+
+                                            {/* lineChart section */}
+                                            <View style={{
+                                                flex: 1,
+                                                alignItems: 'center',
+                                            }}>
+                                                <LineChart
+                                                    withVerticalLabels={false}
+                                                    withHorizontalLabels={false}
+                                                    withDots={false}
+                                                    withInnerLines={false}
+                                                    withVerticalLines={false}
+                                                    withOuterLines={false}
+                                                    data={{
+                                                        datasets: [
+                                                            {
+                                                                data: item.sparkline_in_7d.price
+                                                            }
+                                                        ]
+                                                    }}
+                                                    width={100}
+                                                    height={60}
+                                                    chartConfig={{
+                                                        color: () => priceColor
+                                                    }}
+                                                    bezier
+                                                    style={{
+                                                        paddingRight: 0
+                                                    }}
+                                                />
+                                            </View>
+
+                                            {/* figures section */}
+                                        </View>
+                                    );
+                                }}
+                            />
+                        </View>
+                    );
+                }}
+            />
+
+        );
+    };
+
     return (
         <MainLayout>
             <View style={{
@@ -110,6 +222,7 @@ const Market = ({ getCoinMarket, coins }) => {
                 {renderButtons()}
 
                 {/* MarketList */}
+                {renderList()}
             </View>
         </MainLayout>
     );
