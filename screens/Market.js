073302493eb19fa1c0f07cd1926330f968e50e05
Richard Ingwe <richardingwe5@gmail.com>
2022-05-11 10:08:25 +0100

    worked on Text Buttons

--- screens/Market.js
+++ screens/Market.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { createRef, useEffect } from 'react';
 import {
     View,
     Text,
@@ -8,16 +8,91 @@ import {
     Image
 } from 'react-native';
 import { connect } from "react-redux";
-import { HeaderBar } from "../components";
-import { COLORS } from "../constants";
+import { HeaderBar, TextButton } from "../components";
+import { COLORS, constants, FONTS, SIZES } from "../constants";
 import { getCoinMarket } from "../stores/market/marketActions";
 import MainLayout from "./MainLayout";
+
+const marketTabs = constants.marketTabs.map((marketTab) => ({
+    ...marketTab,
+    ref: createRef
+}));
+
+const Tabs = () => {
+    return (
+        <View style={{
+            flexDirection: 'row'
+        }}>
+
+            {/* Tabs */}
+            {marketTabs.map((item, index) => {
+                return (
+                    <TouchableOpacity
+                        key={`MarketTab-${index}`}
+                        style={{ flex: 1 }}
+                    // onPress
+                    >
+                        <View
+                            ref={item.ref}
+                            style={{
+                                paddingHorizontal: 15,
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: 40
+                            }}
+                        >
+                            <Text style={{
+                                color: COLORS.white,
+                                ...FONTS.h3
+                            }}>{item.title}</Text>
+                        </View>
+                    </TouchableOpacity>
+                );
+            })}
+        </View>
+    );
+};
 
 
 const Market = ({ getCoinMarket, coins }) => {
     useEffect(() => {
         getCoinMarket();
     }, []);
+
+    const renderTabBar = () => (
+        <View style={{
+            marginTop: SIZES.radius,
+            marginHorizontal: SIZES.radius,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.gray
+        }}>
+            <Tabs />
+        </View>
+    );
+
+    const renderButtons = () => (
+        <View style={{
+            flexDirection: 'row',
+            marginTop: SIZES.radius,
+            marginHorizontal: SIZES.radius
+        }}>
+            <TextButton label='USD' />
+
+            <TextButton
+                label='% (7d)'
+                containerStyle={{
+                    marginLeft: SIZES.base
+                }}
+            />
+
+            <TextButton
+                label='Top'
+                containerStyle={{
+                    marginLeft: SIZES.base
+                }}
+            />
+        </View>
+    );
 
     return (
         <MainLayout>
@@ -29,8 +104,10 @@ const Market = ({ getCoinMarket, coins }) => {
                 <HeaderBar title='Market' />
 
                 {/* Tab bar */}
+                {renderTabBar()}
 
                 {/* Buttons */}
+                {renderButtons()}
 
                 {/* MarketList */}
             </View>
